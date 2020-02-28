e1fb05bd8c00a23c90b8377a0d50b016d0766b5b
benjamin zamudio olguin <mrnobody007@benjamins-MacBook-Pro.local>
2020-02-27 21:43:10 -0600

    modificaciones en las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,24 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefounbdComponent } from './shared/nopagefounbd/nopagefounbd.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefounbdComponent,
-    DashboardComponent,
-    Graficas1Component,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
